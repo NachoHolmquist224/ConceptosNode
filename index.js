44ef18a9c639ea721f848a323e750d7f846d2130
Ignacio <ignacioholmquist12@gmail.com>
2026-06-20 19:08:35 -0300

    agregamos commits

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import express from "express"
+import cors from "cors"
 
 const app = express();
 const PORT = 3000;
+
+//MIDDLEWARES
+app.use(cors()) //permite conexiones desde cualquier origen
+app.use(express.json()) //permite recibir datos en formato JSON
+
+//ENDPOINTS
 
 console.log("El back ya escucha 🤩")
 
