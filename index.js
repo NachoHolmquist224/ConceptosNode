7c91761901fe226546de15142ba8f0e601814d7c
Ignacio <ignacioholmquist12@gmail.com>
2026-06-21 11:12:15 -0300

    Agg un archivo estatico de bienvenida

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import {dirname} from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const PORT = 3000;
@@ -9,6 +11,10 @@ const PORT = 3000;
 app.use(cors()); //permite conexiones desde cualquier origen
 app.use(express.json()); //permite recibir datos en formato JSON
 app.use(morgan("dev")); //muestra en consola las peticiones realizadas al servidor
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+console.log(__dirname + "/public"); //muestra la ruta del directorio actual
+app.use(express.static(__dirname + "/public")); //sirve archivos estáticos desde el directorio "public"
 
 //ENDPOINTS
 app.get("/api/saludo", (req, res) => {
